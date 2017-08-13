3ee2962d436587d887d9a433e9792619766ab9e1
cry004 <cry004@gmail.com>
2017-08-13 22:02:10 +0900

    Setup start script and Heroku port

--- node-web-server/server.js
+++ node-web-server/server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
+
+const port = process.env.PORT || 8000;
 var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials')
@@ -54,6 +56,6 @@ app.get('/about',(req, res) => {
   });
 })
 
-app.listen(8000, () => {
-  console.log('server is fired on port 8000')
+app.listen(port, () => {
+  console.log(`server is fired on port ${port}`)
 });
